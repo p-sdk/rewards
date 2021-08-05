0c11c118eb15a0faf5a03fa8f7fcfa3b481d2804
p-sdk <paul.sdk@icloud.com>
2021-08-05 13:54:28 +0200

    Add name to User

--- lib/rewards_app/users/user.ex
+++ lib/rewards_app/users/user.ex
@@ -1,10 +1,21 @@
 defmodule RewardsApp.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
+    field :name, :string
+
     pow_user_fields()
 
     timestamps()
+  end
+
+  def changeset(user_or_changeset, attrs) do
+    user_or_changeset
+    |> pow_changeset(attrs)
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
   end
 end
