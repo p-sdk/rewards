fc7c134e93780a7171573931212a77a732363141
p-sdk <paul.sdk@icloud.com>
2021-08-09 14:57:18 +0200

    Add create_admin/1, set_admin_role/1, is_admin?/1

--- lib/rewards_app/users.ex
+++ lib/rewards_app/users.ex
@@ -8,9 +8,30 @@ defmodule RewardsApp.Users do
 
   alias RewardsApp.Users.User
 
+  @type t :: %User{}
+
   def list_members do
     Repo.all(User)
   end
 
   def get_member!(id), do: Repo.get!(User, id)
+
+  @spec create_admin(map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def create_admin(params) do
+    %User{}
+    |> User.changeset(params)
+    |> User.changeset_role(%{role: "admin"})
+    |> Repo.insert()
+  end
+
+  @spec set_admin_role(t()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def set_admin_role(user) do
+    user
+    |> User.changeset_role(%{role: "admin"})
+    |> Repo.update()
+  end
+
+  @spec is_admin?(t()) :: boolean()
+  def is_admin?(%{role: "admin"}), do: true
+  def is_admin?(_any), do: false
 end
--- test/rewards_app/users_test.exs
+++ test/rewards_app/users_test.exs
@@ -1,7 +1,7 @@
 defmodule RewardsApp.UsersTest do
   use RewardsApp.DataCase
   import RewardsApp.Fixtures
-  alias RewardsApp.Users
+  alias RewardsApp.{Repo, Users, Users.User}
 
   describe "members" do
     test "list_members/0 returns all members" do
@@ -12,6 +12,38 @@ defmodule RewardsApp.UsersTest do
     test "get_member!/1 returns the member with given id" do
       member = user_fixture()
       assert Users.get_member!(member.id) == member
+    end
+  end
+
+  describe "admins" do
+    @valid_params %{
+      name: "User",
+      email: "test@example.com",
+      password: "secret1234",
+      password_confirmation: "secret1234"
+    }
+
+    test "create_admin/1" do
+      assert {:ok, user} = Users.create_admin(@valid_params)
+      assert user.role == "admin"
+    end
+
+    test "set_admin_role/1" do
+      assert {:ok, user} = Repo.insert(User.changeset(%User{}, @valid_params))
+      assert user.role == "member"
+
+      assert {:ok, user} = Users.set_admin_role(user)
+      assert user.role == "admin"
+    end
+
+    test "is_admin?/1" do
+      refute Users.is_admin?(nil)
+
+      assert {:ok, user} = Repo.insert(User.changeset(%User{}, @valid_params))
+      refute Users.is_admin?(user)
+
+      assert {:ok, admin} = Users.create_admin(%{@valid_params | name: "Admin", email: "test2@example.com"})
+      assert Users.is_admin?(admin)
     end
   end
 end
