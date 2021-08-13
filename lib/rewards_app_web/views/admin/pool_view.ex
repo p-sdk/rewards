876d3f7e4d78b1dc7cc4c611ca62ab557abc3194
p-sdk <paul.sdk@icloud.com>
2021-08-13 17:02:23 +0200

    Use select input for months

--- lib/rewards_app_web/views/admin/pool_view.ex
+++ lib/rewards_app_web/views/admin/pool_view.ex
@@ -1,6 +1,25 @@
 defmodule RewardsAppWeb.Admin.PoolView do
   use RewardsAppWeb, :view
 
+  @months [
+    {"January", "1"},
+    {"February", "2"},
+    {"March", "3"},
+    {"April", "4"},
+    {"May", "5"},
+    {"June", "6"},
+    {"July", "7"},
+    {"August", "8"},
+    {"September", "9"},
+    {"October", "10"},
+    {"November", "11"},
+    {"December", "12"}
+  ]
+
+  def month_options() do
+    @months
+  end
+
   def pool_period(pool) do
     Date.new!(pool.year, pool.month, 1)
     |> Calendar.strftime("%B %Y")
