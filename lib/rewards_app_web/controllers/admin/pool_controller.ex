4b4b57086c4b2660e201a416885ea96d07793daa
p-sdk <paul.sdk@icloud.com>
2021-08-16 14:23:00 +0200

    Add assign_member plug to PoolController

--- lib/rewards_app_web/controllers/admin/pool_controller.ex
+++ lib/rewards_app_web/controllers/admin/pool_controller.ex
@@ -3,22 +3,31 @@ defmodule RewardsAppWeb.Admin.PoolController do
 
   alias RewardsApp.{Rewards.Pool, Rewards, Users}
 
-  def index(conn, %{"member_id" => id}) do
-    member = Users.get_member!(id)
+  plug :assign_member
+
+  defp assign_member(conn, _) do
+    member =
+      conn.params["member_id"]
+      |> Users.get_member!()
+
+    assign(conn, :member, member)
+  end
+
+  def index(conn, _params) do
+    member = conn.assigns.member
     pools = Rewards.list_pools_for(member)
 
-    render(conn, "index.html", member: member, pools: pools)
+    render(conn, "index.html", pools: pools)
   end
 
-  def new(conn, %{"member_id" => member_id}) do
-    member = Users.get_member!(member_id)
+  def new(conn, _params) do
     changeset = Rewards.change_pool(%Pool{})
-    render(conn, "new.html", member: member, changeset: changeset)
+    render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"member_id" => member_id, "pool" => pool_params}) do
-    member = Users.get_member!(member_id)
-    pool_params = Map.put(pool_params, "owner_id", member_id)
+  def create(conn, %{"pool" => pool_params}) do
+    member = conn.assigns.member
+    pool_params = Map.put(pool_params, "owner_id", member.id)
 
     case Rewards.create_pool(pool_params) do
       {:ok, _pool} ->
@@ -27,23 +36,22 @@ defmodule RewardsAppWeb.Admin.PoolController do
         |> redirect(to: Routes.admin_member_pool_path(conn, :index, member))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", member: member, changeset: changeset)
+        render(conn, "new.html", changeset: changeset)
     end
 
     changeset = Rewards.change_pool(%Pool{})
-    render(conn, "new.html", member: member, changeset: changeset)
+    render(conn, "new.html", changeset: changeset)
   end
 
-  def edit(conn, %{"member_id" => member_id, "id" => id}) do
-    member = Users.get_member!(member_id)
+  def edit(conn, %{"id" => id}) do
     pool = Rewards.get_pool!(id)
     changeset = Rewards.change_pool(pool)
 
-    render(conn, "edit.html", member: member, pool: pool, changeset: changeset)
+    render(conn, "edit.html", pool: pool, changeset: changeset)
   end
 
-  def update(conn, %{"member_id" => member_id, "id" => id, "pool" => pool_params}) do
-    member = Users.get_member!(member_id)
+  def update(conn, %{"id" => id, "pool" => pool_params}) do
+    member = conn.assigns.member
     pool = Rewards.get_pool!(id)
 
     case Rewards.update_pool(pool, pool_params) do
@@ -53,7 +61,7 @@ defmodule RewardsAppWeb.Admin.PoolController do
         |> redirect(to: Routes.admin_member_pool_path(conn, :index, member))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", member: member, pool: pool, changeset: changeset)
+        render(conn, "edit.html", pool: pool, changeset: changeset)
     end
   end
 end
